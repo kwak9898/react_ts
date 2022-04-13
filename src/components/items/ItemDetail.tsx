730c58ed5c25acb93cb0da9adae98da783e32ddc
kwaktaemin <kwak9898@github.com>
2022-04-13 15:08:43 +0900

    Item Detail 구현 중

--- src/components/items/ItemDetail.tsx
+++ src/components/items/ItemDetail.tsx
@@ -1,51 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import ItemData from "./ItemData";
+import Idata from "./ItemData";
 import "./item.css";
+import getItemkey from "./ItemData";
+import ItemDataModel from "./ItemModel";
+
+interface Ikey {
+  key: number;
+}
 
 function ItemView() {
-  const [data, setData] = useState({});
-  let { key } = useParams();
+  const [data, setData] = useState<ItemDataModel>(
+    new ItemDataModel("첫번째 아이디", "첫번째 아이템", 10000)
+  );
+
+  let { key }: Ikey = useParams();
   let navigate = useNavigate();
 
   useEffect(() => {
-      setData(getItemkey(key))
-  } [key])
+    setData(getItemkey(key));
+  }, [key]);
 
   return (
-      <>
-      <h2 align="center">제품 상세정보</h2>
+    <>
+      <h2 className="align-center">제품 상세정보</h2>
       <div className="product-view-wrapper">
-          {data ? (
-              <>
-                <div className="product-view-row">
-                    <label>제품 번호</label>
-                    <label>{data.key}</label>
-                </div>
-                <div className="product-view-row">
-                    <label>제품</label>
-                    <label>{data.itemName}</label>
-                </div>
-                <div className="product-view-row">
-                    <label>제품 아이디</label>
-                    <label>{data.id}</label>
-                </div>
-                <div className="product-view-row">
-                    <label>가격</label>
-                    <label>{data.price}</label>
-                </div>
-              </>
-          ) : (
-              "해당 제품을 찾을 수 없습니다."
-          )}
-          <button className="product-view-go-list-btn"
-            onClick={() => navigate(-1)}
-          >
-              목록으로 돌아가기
-          </button>
+        {data ? (
+          <>
+            <div className="product-view-row">
+              <label>제품 번호</label>
+              <label>{data.key}</label>
+            </div>
+            <div className="product-view-row">
+              <label>제품</label>
+              <label>{data.itemName}</label>
+            </div>
+            <div className="product-view-row">
+              <label>제품 아이디</label>
+              <label>{data.id}</label>
+            </div>
+            <div className="product-view-row">
+              <label>가격</label>
+              <label>{data.price}</label>
+            </div>
+          </>
+        ) : (
+          "해당 제품을 찾을 수 없습니다."
+        )}
+        <button
+          className="product-view-go-list-btn"
+          onClick={() => navigate(-1)}
+        >
+          목록으로 돌아가기
+        </button>
       </div>
-      </>
-  )
+    </>
+  );
 }
 
 export default ItemView;
