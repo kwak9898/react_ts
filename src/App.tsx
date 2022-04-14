acb76b3adf51ecb319caf2f5902f9d5cd7451acd
kwaktaemin <kwak9898@github.com>
2022-04-14 14:12:51 +0900

    ItmeCreate -> ItemMain

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ItemMain from "./components/items/ItemMain";
 import ItemCreate from "./components/items/ItemCreate";
 import ItemDetail from "./components/items/ItemDetail";
 import ItemEdit from "./components/items/ItemEdit";
@@ -7,9 +8,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ItemCreate />} />
+        <Route path="/" element={<ItemMain />} />
         <Route path="/item/:id" element={<ItemDetail />} />
         <Route path="/item/:id/edit" element={<ItemEdit />} />
+        <Route path="/item/add" element={<ItemCreate />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/items/ItemMain.tsx
+++ src/components/items/ItemMain.tsx
@@ -14,7 +14,7 @@ interface IitemInput {
   itemName: string;
 }
 
-function ItemCreate() {
+function ItemMain() {
   const [itemList, setItemList] = useRecoilState(itemListState);
   const [input, setInput] = useState<IitemInput>({
     itemName: "",
@@ -78,4 +78,4 @@ function ItemCreate() {
     </>
   );
 }
-export default ItemCreate;
+export default ItemMain;
