95f21f10959e704c7f00d6bb20935d0fffa7f4b7
kwaktaemin <kwak9898@github.com>
2022-04-14 09:44:51 +0900

    수정 기능 구현 중

--- src/components/commons/CommonTableColumn.tsx
+++ src/components/commons/CommonTableColumn.tsx
@@ -1,6 +1,6 @@
 function CommonTableColumn(props: {
   children: JSX.Element | JSX.Element[] | string;
-}) {
+}): JSX.Element {
   return <td className="common-table-column">{props.children}</td>;
 }
 
--- src/components/items/ItemList.tsx
+++ src/components/items/ItemList.tsx
@@ -6,16 +6,32 @@ import CommonTableRow from "../commons/CommonTableRow";
 import ItemDataModel from "./ItemModel";
 
 function ItemList(props: { itemList: ItemDataModel[] }) {
+  const [dataEdit, setDataEdit] = useState<ItemDataModel>({
+    id: "",
+    itemName: "",
+    price: 0,
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setDataEdit(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <CommonTable headerList={["제품아이디", "제품명", "가격"]}>
       {props.itemList.map((item, key) => {
         return (
           <CommonTableRow key={key}>
-            <CommonTableColumn>{item.id}</CommonTableColumn>
-            <CommonTableColumn>
+            <CommonTableColumn onchange={onChange}>{item.id}</CommonTableColumn>
+            <CommonTableColumn onchange={onChange}>
               <Link to={`/item/${item.id}`}>{item.itemName}</Link>
             </CommonTableColumn>
-            <CommonTableColumn>{String(item.price)}</CommonTableColumn>
+            <CommonTableColumn onchange={onChange}>
+              {String(item.price)}
+            </CommonTableColumn>
             <CommonTableColumn>
               <Link to={`/item/${item.id}/edit`}>수정</Link>
             </CommonTableColumn>
